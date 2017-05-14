7e1cad614b628e7ab17b1111335ba196358840fa
leoderoko <leoderoko@gmail.com>
2017-05-14 22:02:30 +0500

    Move var

--- watcher/agent.go
+++ watcher/agent.go
@@ -29,51 +29,6 @@ type Agent struct {
 	lastGuids []string
 }
 
-var (
-	AgentsCollection = []Agent{
-		Agent{
-			Type:     "movies",
-			Endpoint: "http://rss.weburg.net/movies/all.rss",
-			Interval: time.Minute,
-			Channel: telebot.Chat{
-				Type:     "channel",
-				Username: "weburg_movies",
-			},
-			CacheSize: 3,
-		},
-		Agent{
-			Type:     "music",
-			Endpoint: "http://rss.weburg.net/music/all.rss",
-			Interval: time.Minute,
-			Channel: telebot.Chat{
-				Type:     "channel",
-				Username: "weburg_music",
-			},
-			CacheSize: 3,
-		},
-		Agent{
-			Type:     "news",
-			Endpoint: "http://rss.weburg.net/news/all.rss",
-			Interval: time.Minute,
-			Channel: telebot.Chat{
-				Type:     "channel",
-				Username: "weburg_times",
-			},
-			CacheSize: 10,
-		},
-		Agent{
-			Type:     "series",
-			Endpoint: "http://rss.weburg.net/movies/series.rss",
-			Interval: time.Minute,
-			Channel: telebot.Chat{
-				Type:     "channel",
-				Username: "weburg_series",
-			},
-			CacheSize: 2,
-		},
-	}
-)
-
 func (a *Agent) CanPost(item rss.Item) bool {
 	for _, guid := range a.lastGuids {
 		if item.GUID == guid {
--- watcher/watcher.go
+++ watcher/watcher.go
@@ -1,6 +1,56 @@
 package watcher
 
-import "sync"
+import (
+	"sync"
+	"time"
+
+	"github.com/tucnak/telebot"
+)
+
+var (
+	AgentsCollection = []Agent{
+		Agent{
+			Type:     "movies",
+			Endpoint: "http://rss.weburg.net/movies/all.rss",
+			Interval: time.Minute,
+			Channel: telebot.Chat{
+				Type:     "channel",
+				Username: "weburg_movies",
+			},
+			CacheSize: 3,
+		},
+		Agent{
+			Type:     "music",
+			Endpoint: "http://rss.weburg.net/music/all.rss",
+			Interval: time.Minute,
+			Channel: telebot.Chat{
+				Type:     "channel",
+				Username: "weburg_music",
+			},
+			CacheSize: 3,
+		},
+		Agent{
+			Type:     "news",
+			Endpoint: "http://rss.weburg.net/news/all.rss",
+			Interval: time.Minute,
+			Channel: telebot.Chat{
+				Type:     "channel",
+				Username: "weburg_times",
+			},
+			CacheSize: 10,
+		},
+		Agent{
+			Type:     "series",
+			Endpoint: "http://rss.weburg.net/movies/series.rss",
+			Interval: time.Minute,
+			Channel: telebot.Chat{
+				Type:     "channel",
+				Username: "weburg_series",
+			},
+			CacheSize: 2,
+		},
+	}
+)
 
 type Watcher struct {
 	Telegram Telegram
